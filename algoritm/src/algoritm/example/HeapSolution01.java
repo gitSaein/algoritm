3caba4f845f448525eac332a26370f7b42395c77
saeinlee <gitsaein@gmail.com>
2022-02-27 15:55:53 +0900

    힙 문제: https://programmers.co.kr/learn/courses/30/lessons/42626

---
+++ algoritm/src/algoritm/example/HeapSolution01.java
@@ -0,0 +1,25 @@
+package algoritm.example;
+import java.util.PriorityQueue;
+
+
+public class HeapSolution01 {
+    public int solution(int[] scoville, int K) {
+
+    	int answer = 0;
+    	PriorityQueue<Integer> q = new PriorityQueue<>();
+    	for(int s : scoville) {
+    		q.add(s);
+    	}
+    	
+    	while(q.peek() < K) {
+    		if(q.size() == 1) return -1;
+            int first = q.poll();
+            int second = q.poll();
+            int mix = first + 2*second;
+    		q.add(mix);
+            answer+=1;
+    	}
+    	
+    	return answer;
+    }
+}
